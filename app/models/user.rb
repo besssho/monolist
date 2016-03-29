3bac6eea540d268203a70bbc31429514de022f1c
besssho <paruttaboy.21@gmail.com>
2016-03-29 15:14:12 +0000

    have_want_btn_add

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,10 @@ class User < ActiveRecord::Base
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
   
-  has_many :wants , foreign_key: "user_id", dependent:destroy
+  has_many :wants ,class_name: "Want" , foreign_key: "user_id", dependent: :destroy
   has_many :want_items, through: :wants, source: :item
   
-  has_many :haves , foreign_key: "user_id", dependent:destroy
+  has_many :haves ,class_name: "Have" , foreign_key: "user_id", dependent: :destroy
   has_many :have_items, through: :haves, source: :item
 
 
@@ -37,11 +37,11 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
-    haves.find_or_create_by(item_id: item.id)
+    haves.create(item_id: item.id)
   end
 
   def unhave(item)
-    haves.find_by(item_id: :item.id).destroy
+    haves.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
@@ -49,7 +49,7 @@ class User < ActiveRecord::Base
   end
 
   def want(item)
-    wants.find_or_create_by(item_id: :item.id)
+    wants.create(item_id: item.id)
   end
 
   def unwant(item)
